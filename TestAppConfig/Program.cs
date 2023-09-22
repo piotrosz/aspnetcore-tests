aec0004beafbe41b684f784a390b1c1002b10298
Piotr Ludwiczuk <pilu@foss.pl>
2023-09-22 13:39:34 +0200

    vault

--- TestAppConfig/Program.cs
+++ TestAppConfig/Program.cs
@@ -3,8 +3,12 @@ using Microsoft.FeatureManagement;
 using Microsoft.FeatureManagement.FeatureFilters;
 using TestAppConfig;
 using TestAppConfig.FeatureFlags;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add Azure App Config
 string connectionString = builder.Configuration.GetConnectionString("AppConfig");
