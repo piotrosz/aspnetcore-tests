c50abd8ccbfc9fbfeb95e256ccfd72607736d0a0
Piotr Ludwiczuk <pilu@foss.pl>
2023-09-22 14:40:21 +0200

    update

--- TestAppConfig/Program.cs
+++ TestAppConfig/Program.cs
@@ -11,18 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add Azure App Config
-//string connectionString = builder.Configuration.GetConnectionString("AppConfig");
+string connectionString = builder.Configuration.GetConnectionString("AppConfig");
 
-//builder.Configuration.AddAzureAppConfiguration(options =>
-//{
-//    options.Connect(connectionString)
-//        // Load all keys that start with `TestApp:` and have no label
-//        .Select("TestApp:*")
-//        // Configure to reload configuration if the registered sentinel key is modified
-//        .ConfigureRefresh(refreshOptions =>
-//            refreshOptions.Register("TestApp:Settings:Sentinel", refreshAll: true))
-//        .UseFeatureFlags(featureFlagOptions => { featureFlagOptions.CacheExpirationInterval = TimeSpan.FromMinutes(1); });
-//});
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(connectionString)
+        // Load all keys that start with `TestApp:` and have no label
+        .Select("TestApp:*")
+        // Configure to reload configuration if the registered sentinel key is modified
+        .ConfigureRefresh(refreshOptions =>
+            refreshOptions.Register("TestApp:Settings:Sentinel", refreshAll: true))
+        .UseFeatureFlags(featureFlagOptions => { featureFlagOptions.CacheExpirationInterval = TimeSpan.FromMinutes(1); });
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -42,15 +42,15 @@ builder.Services.AddAzureAppConfiguration();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 // Use Azure App Configuration middleware for dynamic configuration refresh.
-//app.UseAzureAppConfiguration();
+app.UseAzureAppConfiguration();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
