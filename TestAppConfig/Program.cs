52921b1b60c6f5d678cfce207129ea7216674326
Piotr Ludwiczuk <pilu@foss.pl>
2023-09-22 14:00:02 +0200

    No custom error

--- TestAppConfig/Program.cs
+++ TestAppConfig/Program.cs
@@ -44,12 +44,12 @@ builder.Services.AddAzureAppConfiguration();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 // Use Azure App Configuration middleware for dynamic configuration refresh.
 app.UseAzureAppConfiguration();
